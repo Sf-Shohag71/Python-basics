3d46c2e72abb3ed652c00b2c07a1f1904906b0d2
Sf-Shohag71 <shakhfarid71@gmail.com>
2025-09-08 16:48:54 +0600

    Practice set and ditionary

--- dictionary_lec4.py
+++ dictionary_lec4.py
@@ -24,8 +24,23 @@ student2 = {
         "chemistry": 88
     }
 }
-print(student2["subjects"]["math"])
-print(student2["name"])
-print(student2.get("name"))
-print(student2.get("name1"))
-print(list(student2.keys()))
+# print(student2["subjects"]["math"])
+# print(student2["name"])
+# print(student2.get("name"))
+# print(student2.get("name1"))
+# print(list(student2.keys()))
+
+# Write a program to collect subjects and marks of student from user and store them in a dictionary.
+subject1 = input("Enter subject 1: ")
+marks1 = input("Enter {subject1} marks: ")
+subject2 = input("Enter subject 2: ")
+marks2 = input("Enter {subject2} marks: ")
+subject3 = input("Enter subject 3: ")
+marks3 = input("Enter {subject3} marks: ")
+
+marksheet = {}
+marksheet[subject1] = marks1
+marksheet[subject2] = marks2
+marksheet[subject3] = marks3
+
+print(marksheet)
---
+++ set_lec4.py
@@ -0,0 +1,17 @@
+collection = {1, 2, 2, 2, "hello", "world", "world", 4}
+collection.add(5)
+collection.remove(2)
+collection.add((1, 2, 3))
+# collection.add([1, 2, 3])
+
+# print(collection)
+
+empty_dict = {}
+empty_set = set()
+# print("1. Set or Dictionary?", type(empty_set))
+# print("2. Set or Dictionary?", type(empty_dict))
+# print(type(collection))
+# print(len(collection))
+
+subjects = {"Python", "Java", "JavaScript", "C#", "C++", "Python", "Java", "JavaScript"}
+print(len(subjects))
