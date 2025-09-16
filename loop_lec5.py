49241cbde31e46d87037145b15e037c882a16b8a
Sf-Shohag71 <shakhfarid71@gmail.com>
2025-09-16 16:45:37 +0600

    loop practic

--- loop_lec5.py
+++ loop_lec5.py
@@ -1,6 +1,11 @@
 # Multiplication Table
-n = int(input("Enter a number to see its multiplication table: "))
-x = 1
-while x <= 10:
-    print(f"{n} * {x} = {n * x}")
-    x += 1
+# n = int(input("Enter a number to see its multiplication table: "))
+# x = 1
+# while x <= 10:
+#     print(f"{n} * {x} = {n * x}")
+#     x += 1
+
+# for i in range(1, 101):
+#     for j in range(1, 11):
+#         print(f"{i} * {j} = {i * j}")
+#     print()
