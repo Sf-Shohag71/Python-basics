dc5f4a36a0bf76e85db989f8899f9921e283c074
Sf-Shohag71 <shakhfarid71@gmail.com>
2025-09-28 16:58:53 +0600

    Learn about I/O

--- I_O_lec7.py
+++ I_O_lec7.py
@@ -1,5 +1,21 @@
-f = open("demo.txt", "r")
-data = f.read()
-print(data)
-print(type(data))
-f.close()
+# f = open("demo.txt", "a")
+# data = f.read()
+# line1 = f.readline()
+# print(data)
+# print(line1)
+# print(type(data))
+
+# f.write("This is me")
+# f.write("\nWhat is your name?")
+# f.close()
+
+with open("demo.txt", "r") as f:
+    data = f.read()
+    print(data)
+    
+with open("demo.txt", "w") as f:
+    f.write("This is me --- IGNORE ---")
+
+# delete file
+import os
+os.remove("demo.txt")
